3e90669236faa9fcd2246ade05f4b5a4d66bbeda
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2022-04-20 18:34:41 +0300

    done
    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.styled.jsx';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Container } from 'App.styled';
 import { MyForm } from 'components/Form/Form.jsx';
 import { Contacts } from 'components/Contacts/Contacts.jsx';
@@ -9,86 +9,77 @@ import { v4 as uuidv4 } from 'uuid';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  notify = () =>
+const defaultContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+const derivedContact = JSON.parse(localStorage.getItem('contacts'));
+
+const App = () => {
+  const [contacts, setContacts] = useState(
+    () => derivedContact ?? defaultContacts,
+  );
+  const [filter, setFilter] = useState('');
+
+  const notify = () =>
     toast.warn('That NAME or NUMBER already exist', {
       position: toast.POSITION.TOP_CENTER,
     });
-  isContactDubled = (arr, data, key) => {
+  const isContactDubled = (arr, data, key) => {
     return arr.some(
       contact =>
         contact[key].toLocaleLowerCase() === data[key].toLocaleLowerCase(),
     );
   };
-  onFormSubmit = data => {
+  const onFormSubmit = data => {
     if (
-      this.isContactDubled(this.state.contacts, data, 'name') ||
-      this.isContactDubled(this.state.contacts, data, 'number')
+      isContactDubled(contacts, data, 'name') ||
+      isContactDubled(contacts, data, 'number')
     ) {
-      this.notify();
+      notify();
       return;
     }
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { id: uuidv4(), ...data }],
-    }));
+    setContacts(prevState => [...prevState, { id: uuidv4(), ...data }]);
   };
-  filterHendle = data => {
-    this.setState({ filter: data });
+  const filterHendle = data => {
+    setFilter(data);
   };
-  handleDelete = e => {
-    const chengedContacts = this.state.contacts.filter(
+  const handleDelete = e => {
+    const chengedContacts = contacts.filter(
       contact => contact.id !== e.currentTarget.parentElement.id,
     );
-    this.setState({ contacts: chengedContacts });
-  };
-  componentDidMount = () => {
-    const derivedContact = JSON.parse(localStorage.getItem('contacts'));
-    if (derivedContact !== null && derivedContact !== '') {
-      this.setState({ contacts: derivedContact });
-    }
-  };
-  componentDidUpdate = (_, prevState) => {
-    if (prevState.contacts !== this.state.contacts) {
-    }
-    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    setContacts(chengedContacts);
   };
 
-  render() {
-    const filteredContacts = this.state.contacts.filter(contact => {
-      return contact.name
-        .toLocaleLowerCase()
-        .includes(this.state.filter.toLocaleLowerCase());
-    });
-    return (
-      <Container>
-        <h1>Phonebook</h1>
-        <MyForm
-          onSubmit={this.onFormSubmit}
-          contacts={this.state.contacts}
-          isContDubled={this.isContactDubled}
-        ></MyForm>
-        <ToastContainer />
-        <h2>Contacts:</h2>
-        <Filter onFilter={this.filterHendle}></Filter>
-        <Contacts
-          contacts={
-            this.state.filter !== '' ? filteredContacts : this.state.contacts
-          }
-          onDelete={this.handleDelete}
-        ></Contacts>
-      </Container>
-    );
-  }
-}
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const filteredContacts = contacts.filter(contact => {
+    return contact.name
+      .toLocaleLowerCase()
+      .includes(filter.toLocaleLowerCase());
+  });
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+      <MyForm
+        onSubmit={onFormSubmit}
+        contacts={contacts}
+        isContDubled={isContactDubled}
+      ></MyForm>
+      <ToastContainer />
+      <h2>Contacts:</h2>
+      <Filter onFilter={filterHendle}></Filter>
+      <Contacts
+        contacts={filter !== '' ? filteredContacts : contacts}
+        onDelete={handleDelete}
+      ></Contacts>
+    </Container>
+  );
+};
 
 export default App;
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import { ThemeProvider } from '@emotion/react';
+// import { ThemeProvider } from '@emotion/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-const myTheme = {
-  background: '',
-  violet: '',
-  shadow: '',
-};
+// const myTheme = {
+//   background: '',
+//   violet: '',
+//   shadow: '',
+// };
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={myTheme}>
-      <App />
-    </ThemeProvider>
+    {/* <ThemeProvider theme={myTheme}> */}
+    <App />
+    {/* </ThemeProvider> */}
   </React.StrictMode>,
-  document.getElementById('root'),
 );
